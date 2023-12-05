7ab56180f8f42047365421d8577e1316f99ad1a5
afaryn <audy.fitri10@gmail.com>
2023-12-05 23:38:17 +0700

    sign in update

---
+++ app/src/main/java/com/example/isable_capstone/ui/authentication/sign_up/sign_up_activity.kt
@@ -0,0 +1,12 @@
+package com.example.isable_capstone.ui.authentication.sign_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.isable_capstone.R
+
+class sign_up_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
