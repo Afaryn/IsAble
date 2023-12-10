c8c0588b2c16418284106190ef03dba091d38d8c
Galuhika <galuhika777@gmail.com>
2023-12-10 22:43:41 +0700

    sign in & sign up

--- app/src/main/java/com/example/isable_capstone/ui/authentication/sign_in/sign_in_activity.kt
+++ app/src/main/java/com/example/isable_capstone/ui/authentication/sign_in/sign_in_activity.kt
@@ -5,31 +5,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.isable_capstone.MainActivity
 import com.example.isable_capstone.R
 import com.example.isable_capstone.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class sign_in_activity : AppCompatActivity() {
 
-private lateinit var binding: ActivitySignInBinding
+    private lateinit var binding: ActivitySignInBinding
+    lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val toolbar = findViewById<Toolbar>(R.id.xml_toolbar)
+        auth = FirebaseAuth.getInstance()
 
         setSupportActionBar(toolbar)
         supportActionBar?.title=getString(R.string.sign_in)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.btnSignIn.setOnClickListener{
-            startActivity(Intent(this@sign_in_activity, MainActivity::class.java))
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            LoginFirebase(email, pass)
+//            startActivity(Intent(this@sign_in_activity, MainActivity::class.java))
         }
-
-
     }
+
+    private fun LoginFirebase(email: String, pass: String) {
+        auth.signInWithEmailAndPassword(email, pass)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Berhasil", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 
     @Suppress("DEPRECATION")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
